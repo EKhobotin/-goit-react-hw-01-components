d13a58a9deec3efb8fd5b233f0be8d53c19f9d4b
EKhobotin <evgen.29.3.85@gmail.com>
2023-12-07 19:26:15 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import { Profile } from "./Profile/Profile";
 import userData from '../data/user.json';
 
-console.log(userData);
 const { username, tag, location, avatar, stats } = userData
 
 export const App = () => {
